4c678c83b6d425fca84df7fba86b32731359784d
Louis Popovic <louispop@gmail.com>
2023-02-21 10:27:25 -0500

    tp4

--- tp3/dist/src.e31bb0bc.js
+++ tp3/dist/src.e31bb0bc.js
@@ -3377,7 +3377,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62752" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62361" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
